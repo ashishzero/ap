488075a12f3de603f4d61b20364082978f4fe771
Ashish Bhattarai <zeroa5620@gmail.com>
2023-01-23 10:14:59 +0545

    Enbled to create multiple endpoints from multiple threads

--- Source/Main.c
+++ Source/Main.c
@@ -111,7 +111,6 @@ typedef struct PL_AudioDevice {
 	PL_AudioSpec        spec;
 	IAudioClient *      client;
 	IAudioRenderClient *render;
-	volatile LONG       playing;
 	WAVEFORMATEX *      format;
 	char16_t *          id;
 } PL_AudioDevice;
@@ -124,122 +123,42 @@ typedef enum PL_AudioEvent {
 	PL_AudioEvent_EnumMax
 } PL_AudioEvent;
 
+typedef struct PL_AudioEndpoint {
+	PL_AudioDevice     device;
+	HANDLE             events[PL_AudioEvent_EnumMax];
+	PL_AudioUpdateProc update;
+	void *             user;
+	volatile LONG      playing;
+	HANDLE             thread;
+} PL_AudioEndpoint;
+
 static struct {
 	volatile LONG        Initialized;
-	volatile LONG        Guard;
-	HANDLE               Events[PL_AudioEvent_EnumMax];
-	HANDLE               Thread;
 	IMMDeviceEnumerator *DeviceEnumerator;
-	PL_AudioUpdateProc   UpdateProc;
-	void *               UserData;
-	PL_AudioDevice       Device;
-} G;
+} Instance;
 
-static void PL_InitAudioThread(void) {
-	HANDLE thread = GetCurrentThread();
-	SetThreadDescription(thread, L"PL Audio Thread");
-
-	DWORD task_index;
-	AvSetMmThreadCharacteristicsW(L"Pro Audio", &task_index);
-
+static void PL_InitAudioInstance(void) {
 	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 	if (hr != S_OK && hr != S_FALSE && hr != RPC_E_CHANGED_MODE) {
-		FatalAppExitW(0, L"Failed to initialize audio thread");
+		FatalAppExitW(0, L"Failed to initialize audio instance");
 	}
 
-	hr = CoCreateInstance(&PL_CLSID_MMDeviceEnumerator, nullptr, CLSCTX_ALL, &PL_IID_IMMDeviceEnumerator, &G.DeviceEnumerator);
-	if (FAILED(hr)) {
-		FatalAppExitW(0, L"Failed to initialize audio thread");
-	}
-
-	G.Events[PL_AudioEvent_Update] = CreateEventW(nullptr, FALSE, FALSE, nullptr);
-	G.Events[PL_AudioEvent_Play]   = CreateSemaphoreW(nullptr, 0, 255, nullptr);
-	G.Events[PL_AudioEvent_Pause]  = CreateSemaphoreW(nullptr, 0, 255, nullptr);
-	G.Events[PL_AudioEvent_Reset]  = CreateSemaphoreW(nullptr, 0, 255, nullptr);
-
-	for (int i = 0; i < PL_AudioEvent_EnumMax; ++i) {
-		if (!G.Events[i]) {
-			PL_FatalError(L"Failed to start audio thread");
-		}
-	}
-}
-
-static void PL_CloseAudioDevice(void) {
-	if (G.Device.client) IAudioClient_Release(G.Device.client);
-	if (G.Device.render) IAudioRenderClient_Release(G.Device.render);
-	if (G.Device.format) CoTaskMemFree(G.Device.format);
-	if (G.Device.id)     CoTaskMemFree(G.Device.id);
-
-	memset(&G.Device, 0, sizeof(G.Device));
-}
-
-static void PL_DeinitAudioThread(void) {
-	PL_CloseAudioDevice();
-
-	for (int i = 0; i < PL_AudioEvent_EnumMax; ++i) {
-		CloseHandle(G.Events[i]);
-		G.Events[i] = nullptr;
-	}
-
-	if (G.DeviceEnumerator)
-		IMMDeviceEnumerator_Release(G.DeviceEnumerator);
-	G.DeviceEnumerator = nullptr;
-	CoUninitialize();
-}
-
-static void PL_StartAudioDevice(void) {
-	InterlockedExchange(&G.Device.playing, 1);
-
-	HRESULT hr  = IAudioClient_Start(G.Device.client);
-	if (hr == AUDCLNT_E_NOT_STOPPED)
+	if (InterlockedCompareExchange(&Instance.Initialized, 1, 0) == 1) {
+		IMMDeviceEnumerator_AddRef(Instance.DeviceEnumerator);
 		return;
-	if (PL_AudioFailed(hr)) {
-		G.Device.flags |= PL_AudioDevice_IsLost;
+	}
+
+	hr = CoCreateInstance(&PL_CLSID_MMDeviceEnumerator, nullptr, CLSCTX_ALL, &PL_IID_IMMDeviceEnumerator, &Instance.DeviceEnumerator);
+	if (FAILED(hr)) {
+		FatalAppExitW(0, L"Failed to initialize audio instance");
 	}
 }
 
-static void PL_StopAudioDevice(void) {
-	InterlockedExchange(&G.Device.playing, 0);
-
-	HRESULT hr = IAudioClient_Stop(G.Device.client);
-	if (PL_AudioFailed(hr)) {
-		G.Device.flags |= PL_AudioDevice_IsLost;
+static void PL_DeinitAudioInstance(void) {
+	if (IMMDeviceEnumerator_Release(Instance.DeviceEnumerator) == 0) {
+		InterlockedExchange(&Instance.Initialized, 0);
+		CoUninitialize();
 	}
-}
-
-static void PL_ResetAudioDevice(void) {
-	InterlockedExchange(&G.Device.playing, 0);
-
-	PL_StopAudioDevice();
-	HRESULT hr = IAudioClient_Reset(G.Device.client);
-	if (PL_AudioFailed(hr)) {
-		G.Device.flags |= PL_AudioDevice_IsLost;
-	}
-}
-
-static void PL_UpdateAudioDevice(void) {
-	UINT32 padding = 0;
-	HRESULT hr = IAudioClient_GetCurrentPadding(G.Device.client, &padding);
-	if (PL_AudioFailed(hr)) goto failed;
-
-	UINT32 frames = G.Device.frames - padding;
-	BYTE *data    = nullptr;
-	hr = IAudioRenderClient_GetBuffer(G.Device.render, frames, &data);
-	if (PL_AudioFailed(hr)) goto failed;
-
-	u32 written = G.UpdateProc(&G.Device.spec, (Buffer){ .count=frames, .data=data }, G.UserData);
-
-	DWORD flags = 0;
-	if (written < frames)
-		flags |= AUDCLNT_BUFFERFLAGS_SILENT;
-
-	hr = IAudioRenderClient_ReleaseBuffer(G.Device.render, written, flags);
-	if (PL_AudioFailed(hr)) goto failed;
-
-	return;
-
-failed:
-	G.Device.flags |= PL_AudioDevice_IsLost;
 }
 
 static void PL_TranslateSpec(PL_AudioSpec *dst, const WAVEFORMATEX *src) {
@@ -291,93 +210,265 @@ static void PL_TranslateSpec(PL_AudioSpec *dst, const WAVEFORMATEX *src) {
 		if (ext->dwChannelMask & SPEAKER_TOP_BACK_RIGHT)        dst->channels.mask |= PL_AudioChannel_TopBackRight;
 	}
 
-
 	dst->channels.count = src->nChannels;
 	dst->rate.secs      = src->nSamplesPerSec;
-
 }
 
-static void PL_OpenAudioDevice(char16_t *id) {
+static void PL_CloseAudioDevice(PL_AudioDevice *device) {
+	if (device->client) IAudioClient_Release(device->client);
+	if (device->render) IAudioRenderClient_Release(device->render);
+	if (device->format) CoTaskMemFree(device->format);
+	if (device->id)     CoTaskMemFree(device->id);
+	memset(device, 0, sizeof(*device));
+}
+
+static void PL_OpenAudioDevice(PL_AudioDevice *device, char16_t *id, HANDLE event) {
 	HRESULT hr;
 
 	IMMDevice *endpoint = nullptr;
 
 	if (id) {
-		hr = IMMDeviceEnumerator_GetDevice(G.DeviceEnumerator, id, &endpoint);
+		hr = IMMDeviceEnumerator_GetDevice(Instance.DeviceEnumerator, id, &endpoint);
 		if (hr != E_NOTFOUND) {
 			if (PL_Failed(hr)) {
-				G.Device.flags |= PL_AudioDevice_IsLost;
+				device->flags |= PL_AudioDevice_IsLost;
 				return;
 			}
 		}
 	}
 
 	if (!endpoint) {
-		hr = IMMDeviceEnumerator_GetDefaultAudioEndpoint(G.DeviceEnumerator, eRender, eMultimedia, &endpoint);
+		hr = IMMDeviceEnumerator_GetDefaultAudioEndpoint(Instance.DeviceEnumerator, eRender, eMultimedia, &endpoint);
 		if (hr == E_NOTFOUND) {
-			G.Device.flags |= PL_AudioDevice_IsNotPresent;
+			device->flags |= PL_AudioDevice_IsNotPresent;
 			return;
 		}
 	}
 
 	if (PL_Failed(hr)) {
-		G.Device.flags |= PL_AudioDevice_IsNotPresent;
+		device->flags |= PL_AudioDevice_IsNotPresent;
 		return;
 	}
 
-	G.Device.flags = 0;
+	device->flags = 0;
 
-	IMMDevice_GetId(endpoint, &G.Device.id);
+	IMMDevice_GetId(endpoint, &device->id);
 
-	hr = IMMDevice_Activate(endpoint, &PL_IID_IAudioClient, CLSCTX_ALL, nullptr, &G.Device.client);
+	hr = IMMDevice_Activate(endpoint, &PL_IID_IAudioClient, CLSCTX_ALL, nullptr, &device->client);
 	if (PL_AudioFailed(hr)) goto failed;
 
-	hr = IAudioClient_GetMixFormat(G.Device.client, &G.Device.format);
+	hr = IAudioClient_GetMixFormat(device->client, &device->format);
 	if (PL_AudioFailed(hr)) goto failed;
 
 	REFERENCE_TIME device_period;
-	hr = IAudioClient_GetDevicePeriod(G.Device.client, nullptr, &device_period);
+	hr = IAudioClient_GetDevicePeriod(device->client, nullptr, &device_period);
 	if (PL_AudioFailed(hr)) goto failed;
 
-	hr = IAudioClient_Initialize(G.Device.client, AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_EVENTCALLBACK,
-		device_period, device_period, G.Device.format, nullptr);
+	hr = IAudioClient_Initialize(device->client, AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_EVENTCALLBACK,
+		device_period, device_period, device->format, nullptr);
 	Assert(hr != AUDCLNT_E_BUFFER_SIZE_NOT_ALIGNED);
 	if (PL_AudioFailed(hr)) goto failed;
 
-	hr = IAudioClient_SetEventHandle(G.Device.client, G.Events[PL_AudioEvent_Update]);
+	PL_TranslateSpec(&device->spec, device->format);
+
+	hr = IAudioClient_SetEventHandle(device->client, event);
 	if (PL_AudioFailed(hr)) goto failed;
 
-	hr = IAudioClient_GetBufferSize(G.Device.client, &G.Device.frames);
+	hr = IAudioClient_GetBufferSize(device->client, &device->frames);
 	if (PL_AudioFailed(hr)) goto failed;
-
-	hr = IAudioClient_GetService(G.Device.client, &PL_IID_IAudioRenderClient, &G.Device.render);
+	
+	hr = IAudioClient_GetService(device->client, &PL_IID_IAudioRenderClient, &device->render);
 	if (PL_AudioFailed(hr)) goto failed;
 
 	IMMDevice_Release(endpoint);
-
-	PL_TranslateSpec(&G.Device.spec, G.Device.format);
-
-	PL_UpdateAudioDevice();
-	PL_StartAudioDevice();
 
 	return;
 
 failed:
-	PL_CloseAudioDevice();
+	PL_CloseAudioDevice(device);
 	IMMDevice_Release(endpoint);
 
-	G.Device.flags |= PL_AudioDevice_IsLost;
-
-	return;
+	device->flags |= PL_AudioDevice_IsLost;
 }
 
-static void PL_ReloadAudioDeviceIfLost() {
-	if (G.Device.flags & PL_AudioDevice_IsLost) {
-		PL_CloseAudioDevice();
-		PL_OpenAudioDevice(nullptr);
-	} else if (G.Device.flags & PL_AudioDevice_IsNotPresent) {
-		PL_OpenAudioDevice(nullptr);
+static void PL_ImUpdateAudioEndpoint(PL_AudioEndpoint *endpoint) {
+	PL_AudioDevice *device = &endpoint->device;
+
+	UINT32 padding = 0;
+	HRESULT hr = IAudioClient_GetCurrentPadding(device->client, &padding);
+	if (PL_AudioFailed(hr)) goto failed;
+
+	UINT32 frames = device->frames - padding;
+	if (frames == 0) return;
+
+	BYTE *data    = nullptr;
+	hr = IAudioRenderClient_GetBuffer(device->render, frames, &data);
+	if (PL_AudioFailed(hr)) goto failed;
+
+	u32 written = endpoint->update(&device->spec, (Buffer){ .count=frames, .data=data }, endpoint->user);
+
+	DWORD flags = 0;
+	if (written < frames)
+		flags |= AUDCLNT_BUFFERFLAGS_SILENT;
+
+	hr = IAudioRenderClient_ReleaseBuffer(device->render, written, flags);
+	if (PL_AudioFailed(hr)) goto failed;
+
+	return;
+
+failed:
+	device->flags |= PL_AudioDevice_IsLost;
+	InterlockedExchange(&endpoint->playing, 0);
+}
+
+static void PL_ImRecoverAudioDeviceIfLost(PL_AudioEndpoint *endpoint) {
+	PL_AudioDevice *device = &endpoint->device;
+
+	if (device->flags & PL_AudioDevice_IsLost) {
+		PL_CloseAudioDevice(device);
+		PL_OpenAudioDevice(device, nullptr, endpoint->events[PL_AudioEvent_Update]);
+	} else if (device->flags & PL_AudioDevice_IsNotPresent) {
+		PL_OpenAudioDevice(device, nullptr, endpoint->events[PL_AudioEvent_Update]);
 	}
+}
+
+static DWORD WINAPI PL_AudioThread(LPVOID param) {
+	HANDLE thread = GetCurrentThread();
+	SetThreadDescription(thread, L"PL Audio Thread");
+
+	DWORD task_index;
+	HANDLE avrt = AvSetMmThreadCharacteristicsW(L"Pro Audio", &task_index);
+
+	PL_AudioEndpoint *endpoint = param;
+
+	while (1) {
+		DWORD wait = WaitForMultipleObjects(PL_AudioEvent_EnumMax, endpoint->events, FALSE, 200);
+
+		switch (wait) {
+		case WAIT_OBJECT_0 + PL_AudioEvent_Update:
+		{
+			PL_ImUpdateAudioEndpoint(endpoint);
+		} break;
+
+		case WAIT_OBJECT_0 + PL_AudioEvent_Play:
+		{
+			InterlockedExchange(&endpoint->playing, 1);
+			HRESULT hr = IAudioClient_Start(endpoint->device.client);
+			if (hr != AUDCLNT_E_NOT_STOPPED) {
+				if (PL_AudioFailed(hr)) {
+					endpoint->device.flags |= PL_AudioDevice_IsLost;
+					InterlockedExchange(&endpoint->playing, 0);
+				}
+			}
+		} break;
+
+		case WAIT_OBJECT_0 + PL_AudioEvent_Pause:
+		{
+			InterlockedExchange(&endpoint->playing, 0);
+			HRESULT hr = IAudioClient_Stop(endpoint->device.client);
+			if (PL_AudioFailed(hr)) {
+				endpoint->device.flags |= PL_AudioDevice_IsLost;
+			}
+		} break;
+
+		case WAIT_OBJECT_0 + PL_AudioEvent_Reset:
+		{
+			InterlockedExchange(&endpoint->playing, 0);
+			IAudioClient_Stop(endpoint->device.client);
+			HRESULT hr = IAudioClient_Reset(endpoint->device.client);
+			if (PL_AudioFailed(hr)) {
+				endpoint->device.flags |= PL_AudioDevice_IsLost;
+			}
+		} break;
+
+		case WAIT_TIMEOUT:
+		{
+			PL_ImRecoverAudioDeviceIfLost(endpoint);
+		} break;
+		}
+	}
+
+	AvRevertMmThreadCharacteristics(&avrt);
+
+	return 0;
+}
+
+void PL_CloseAudioEndpoint(PL_AudioEndpoint *endpoint) {
+	if (endpoint) {
+		if (endpoint->thread)
+			TerminateThread(endpoint->thread, 0);
+
+		PL_CloseAudioDevice(&endpoint->device);
+
+		for (int i = 0; i < PL_AudioEvent_EnumMax; ++i) {
+			if (endpoint->events[i])
+				CloseHandle(endpoint->events[i]);
+		}
+
+		CoTaskMemFree(endpoint);
+	}
+
+	PL_DeinitAudioInstance();
+}
+
+PL_AudioEndpoint *PL_OpenAudioEndpoint(char16_t *device_id, PL_AudioUpdateProc update, void *user) {
+	PL_InitAudioInstance();
+
+	PL_AudioEndpoint *endpoint = CoTaskMemAlloc(sizeof(PL_AudioEndpoint));
+	if (!endpoint) goto failed;
+
+	memset(endpoint, 0, sizeof(*endpoint));
+
+	const int MAX_EVENTS = 16;
+
+	endpoint->events[PL_AudioEvent_Update] = CreateEventW(nullptr, FALSE, FALSE, nullptr);
+	endpoint->events[PL_AudioEvent_Play]   = CreateSemaphoreW(nullptr, 0, MAX_EVENTS, nullptr);
+	endpoint->events[PL_AudioEvent_Pause]  = CreateSemaphoreW(nullptr, 0, MAX_EVENTS, nullptr);
+	endpoint->events[PL_AudioEvent_Reset]  = CreateSemaphoreW(nullptr, 0, MAX_EVENTS, nullptr);
+	
+	for (int i = 0; i < PL_AudioEvent_EnumMax; ++i) {
+		if (!endpoint->events[i]) {
+			PL_FatalError(L"Failed to open audio endpoint");
+		}
+	}
+
+	endpoint->update = update;
+	endpoint->user   = user;
+
+	PL_OpenAudioDevice(&endpoint->device, device_id, endpoint->events[PL_AudioEvent_Update]);
+
+	endpoint->thread = CreateThread(nullptr, 0, PL_AudioThread, endpoint, 0, nullptr);
+	if (!endpoint->thread) {
+		PL_FatalError(L"Failed to create thread");
+	}
+
+	return endpoint;
+
+failed:
+	PL_CloseAudioEndpoint(endpoint);
+	return nullptr;
+}
+
+bool PL_IsAudioEndpointPlaying(PL_AudioEndpoint *endpoint) {
+	return endpoint->playing != 0;
+}
+
+void PL_UpdateAudioEndpoint(PL_AudioEndpoint *endpoint) {
+	if (!PL_IsAudioEndpointPlaying(endpoint)) {
+		SetEvent(endpoint->events[PL_AudioEvent_Update]);
+	}
+}
+
+void PL_PauseAudioEndpoint(PL_AudioEndpoint *endpoint) {
+	ReleaseSemaphore(endpoint->events[PL_AudioEvent_Pause], 1, nullptr);
+}
+
+void PL_ResumeAudioEndpoint(PL_AudioEndpoint *endpoint) {
+	ReleaseSemaphore(endpoint->events[PL_AudioEvent_Play], 1, nullptr);
+}
+
+void PL_ResetAudioEndpoint(PL_AudioEndpoint *endpoint) {
+	ReleaseSemaphore(endpoint->events[PL_AudioEvent_Reset], 1, nullptr);
 }
 
 //HRESULT ApBackend_QueryInterface(IMMNotificationClient *_this, REFIID riid, void **ppvObject) {
@@ -527,108 +618,6 @@ static void PL_ReloadAudioDeviceIfLost() {
 //	return S_OK;
 //}
 
-static DWORD WINAPI PL_AudioThreadThread(LPVOID param) {
-	PL_InitAudioThread();
-
-	PL_OpenAudioDevice(nullptr);
-
-	while (1) {
-		DWORD wait = WaitForMultipleObjects(PL_AudioEvent_EnumMax, G.Events, FALSE, 200);
-
-		if (wait == WAIT_OBJECT_0 + PL_AudioEvent_Update) {
-			PL_UpdateAudioDevice();
-		} else if (wait == WAIT_OBJECT_0 + PL_AudioEvent_Play) {
-			PL_StartAudioDevice();
-		} else if (wait == WAIT_OBJECT_0 + PL_AudioEvent_Pause) {
-			PL_StopAudioDevice();
-		} else if (wait == WAIT_OBJECT_0 + PL_AudioEvent_Reset) {
-			PL_ResetAudioDevice();
-		} else if (wait == WAIT_TIMEOUT) {
-			PL_ReloadAudioDeviceIfLost();
-		}
-	}
-
-
-	//ApBackend *backend = nullptr;
-	//hr = ApBackend_Create(&backend);
-	//if (FAILED(hr)) {
-	//	FatalAppExitW(0, L"Failed to initialize audio device");
-	//}
-
-	//while (1) {
-	//	static_assert(ApEvent_RequireUpdate == 0 && ApEvent_DeviceChanged == 1,
-	//		"RequireUpdate and DeviceChanged are the only events the audio thread needs to wait for");
-
-	//	DWORD event = WaitForMultipleObjects(2, backend->events, FALSE, INFINITE);
-
-	//	if (event == WAIT_OBJECT_0 + ApEvent_RequireUpdate) {
-	//		ApBackend_Update(backend);
-	//	} else if (event == WAIT_OBJECT_0 + ApEvent_DeviceChanged) {
-	//		ApDevice backup_device                 = backend->devices[ApDeviceKind_Backup];
-	//		backend->devices[ApDeviceKind_Backup]  = backend->devices[ApDeviceKind_Current];
-	//		backend->devices[ApDeviceKind_Current] = backup_device;
-	//		SetEvent(backend->events[ApEvent_DeviceInstalled]);
-	//		ApBackend_Start(backend);
-	//	}
-	//}
-
-	PL_DeinitAudioThread();
-
-	return 0;
-}
-
-//
-//
-//
-
-void PL_SetAudioUpdateProc(PL_AudioUpdateProc proc, void *user) {
-	G.UpdateProc = proc;
-	G.UserData   = user;
-}
-
-void PL_SpawnAudioThread(PL_AudioUpdateProc proc, void *user) {
-	Assert(proc != nullptr);
-
-	if (InterlockedCompareExchange(&G.Initialized, 1, 0)) {
-		PL_SetAudioUpdateProc(proc, user);
-		return;
-	}
-
-	PL_SetAudioUpdateProc(proc, user);
-
-	G.Thread = CreateThread(nullptr, 0, PL_AudioThreadThread, nullptr, 0, nullptr);
-	if (!G.Thread) {
-		PL_FatalError(L"Failed to create thread");
-	}
-}
-
-void PL_KillAudioThread(void) {
-	TerminateThread(G.Thread, 0);
-	G.Thread = nullptr;
-
-	PL_DeinitAudioThread();
-
-	G.UpdateProc  = nullptr;
-	G.UserData    = nullptr;
-	G.Initialized = 0;
-}
-
-bool PL_IsAudioPlaying(void) {
-	return G.Device.playing != 0;
-}
-
-void PL_PauseAudio(void) {
-	ReleaseSemaphore(G.Events[PL_AudioEvent_Pause], 1, nullptr);
-}
-
-void PL_ResumeAudio(void) {
-	ReleaseSemaphore(G.Events[PL_AudioEvent_Play], 1, nullptr);
-}
-
-void PL_ResetAudio(void) {
-	ReleaseSemaphore(G.Events[PL_AudioEvent_Reset], 1, nullptr);
-}
-
 //
 //
 //
@@ -747,7 +736,10 @@ int main(int argc, char *argv[]) {
 
 	CurrentStream = Serialize(audio, &MaxSample);
 
-	PL_SpawnAudioThread(UpdateAudio, nullptr);
+	PL_AudioEndpoint *endpoint = PL_OpenAudioEndpoint(nullptr, UpdateAudio, nullptr);
+
+	PL_UpdateAudioEndpoint(endpoint);
+	PL_ResumeAudioEndpoint(endpoint);
 
 	char input[1024];
 
@@ -756,18 +748,22 @@ int main(int argc, char *argv[]) {
 		int n = scanf_s("%s", input, (int)sizeof(input));
 
 		if (strcmp(input, "resume") == 0) {
-			PL_ResumeAudio();
+			PL_ResumeAudioEndpoint(endpoint);
 		} else if (strcmp(input, "pause") == 0) {
-			PL_PauseAudio();
+			PL_PauseAudioEndpoint(endpoint);
 		} else if (strcmp(input, "stop") == 0) {
-			PL_ResetAudio();
+			PL_ResetAudioEndpoint(endpoint);
 			CurrentSample = 0;
 		} else if (strcmp(input, "reset") == 0) {
-			PL_ResetAudio();
+			PL_ResetAudioEndpoint(endpoint);
 			CurrentSample = 0;
-			PL_ResumeAudio();
+			PL_ResumeAudioEndpoint(endpoint);
+		} else if (strcmp(input, "quit") == 0) {
+			break;
 		}
 	}
+
+	PL_CloseAudioEndpoint(endpoint);
 
 	return 0;
 }
